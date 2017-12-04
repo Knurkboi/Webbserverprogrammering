bc95cf08d107041d03c71c36add956844db8f1a7
Knurkboi <anton.limberg@gmail.com>
2017-12-04 21:31:34 +0100

    Gjort Html-delen av koden
    Vilken agerar som interfacet till inloggningen och skickar vidare inputtade infon till php-koden

---
+++ Uppgifter/Login/login.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        
+        <meta charset="UTF-8">
+        
+        <title>Login asf</title>
+        
+    </head>
+    
+    <body>
+        
+        <form action="submit.php" method="post">
+
+            <p>
+
+                <label for="emailAddress">Email Address:</label>
+
+                <input type="email name" name="email" id="emailAddress">
+
+            </p>
+            
+            <p>
+                <label for="password">Password:</label>
+
+                <input type="password" name="password" id="password">
+
+            </p>
+
+            <input type="submit" value="Submit">
+
+        </form>
+        
+    </body>
+    
+</html>
