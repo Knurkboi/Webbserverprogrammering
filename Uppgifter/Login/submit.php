426068de0904179b05cf11f7e743257351c54a54
Knurkboi <anton.limberg@gmail.com>
2017-12-04 22:06:27 +0100

    Php-koden blev klar fett snabbt boi

--- Uppgifter/Login/submit.php
+++ Uppgifter/Login/submit.php
@@ -2,4 +2,19 @@
 
 $dbc = mysqli_connect("localhost","root","","register");
 
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$query = "SELECT * FROM register WHERE email='$email' AND password='$password';";
+
+$result = mysqli_query($dbc, $query);
+
+if($row = mysqli_fetch_array($result)){
+    echo "inloggad asf boiii";
+}
+
+else{
+    echo "Fel passw grabben oof";
+}
+
 ?>
