b88df7a5cc72b20b4b9f0e7687a54b68fc49a4de
Knurkboi <anton.limberg@gmail.com>
2018-01-20 21:32:02 +0100

    Uppgift 5 / början av uppgift 6
    bigshaq

--- javascript/index.php
+++ javascript/index.php
@@ -9,9 +9,56 @@
         
         <style>
         
-            div {
-                display: inline;
-                margin-left: 20px;
+            #content {
+                display: block;
+            }
+            
+            #uppgift1 {
+                display: block;
+                margin-left: 5px;
+                border: 1px solid black;
+                padding: 10px 10px;
+            }
+            
+            #oof {
+                display: block;
+                margin-left: 5px;
+                border: 1px solid black;
+                padding: 10px 10px;
+            }
+            
+            #bild {
+                width: 128px;
+                height: 128px;
+            }
+            
+            #uppgift3 {
+                display: block;
+                margin-left: 5px;
+                border: 1px solid black;
+                padding: 10px 10px;
+            }
+            
+            #uppgift4 {
+                display: block;
+                margin-left: 5px;
+                border: 1px solid black;
+                padding: 10px 10px;
+            }
+            
+            #text {
+                display: none;
+            }
+            
+            #toggleBtnUppgift5 {
+                margin: 10px 5px;
+            }
+            
+            #uppgift6 {
+                display: block;
+                margin-left: 5px;
+                border: 1px solid black;
+                padding: 10px 10px;
             }
             
         </style>
@@ -20,8 +67,6 @@
 
     <body id="body">
 
-        <button id="alertBtn" type="button" onclick="alertClick()">Alert</button>
-
         <script>
 
         function alertClick() {
@@ -29,19 +74,19 @@
         }
 
         function picHover(x) {
-            x.style.height = "128px";
-            x.style.width = "128px";
+            x.style.height = "256px";
+            x.style.width = "256px";
         }
 
         function picNoHover(x) {
-            x.style.height = "64px";
-            x.style.width = "64px";
+            x.style.height = "128px";
+            x.style.width = "128px";
         }
             
         function picToggle() {
             var x = document.getElementById("oof");
             if (x.style.display === "none") {
-                x.style.display = "block";
+                x.style.display = "inline";
             } else {
                 x.style.display = "none";
             }
@@ -51,26 +96,75 @@
             var x = document.getElementById("body");
             x.style.backgroundColor = "pink";
         }
+            
+        function uppgift5() {
+            textToggle();
+            contentToggle();
+        }
+            
+        function textToggle() {
+            var x = document.getElementById("text");
+            if (x.style.display === "none") {
+                x.style.display = "block";
+            } else {
+                x.style.display = "none";
+            }
+        }    
+            
+        function contentToggle() {
+            var x = document.getElementById("content");
+            if (x.style.display === "block") {
+                x.style.display = "none";
+            } else {
+                x.style.display = "block";
+            }
+        }   
+            
+        function uppgift6() {
+            
+        }    
 
         </script>
 
-        <div id="oof">
-            
-            <img onmouseover="picHover(this)" onmouseout="picNoHover(this)" src="emon.jpg">
-            
-        </div>
-            
-        <div id="toggleImgBtn">
+        <div id="text"><h1>STOR ENAK LITEN beniz</h1></div>
+        
+        <div id="content">
+        
+            <div id="uppgift1">
+
+                <button id="alertBtn" type="button" onclick="alertClick()">Uppgift 1</button>
+
+            </div>
+
+            <div id="oof">
+
+                <img id="bild" onmouseover="picHover(this)" onmouseout="picNoHover(this)" src="emon.jpg">
+
+                <p>Uppgift 2</p>
                 
-            <button id="bildBtn" type="button" onclick="picToggle(this)">bigBenizBoiz</button>
+            </div>
+
+            <div id="uppgift3">
+
+                <button id="bildBtn" type="button" onclick="picToggle(this)">Uppgift 3</button>
+
+            </div>
+
+            <div id="uppgift4">
+
+                <button id="colorBtn" type="button" onclick="bgColorChange(this)">Uppgift 4</button>
+
+            </div>
             
+            <div id="uppgift6">
+            
+                <button id="btn6" type="button" onclick="uppgift6">Uppgift 6</button>
+                
+            </div>
+        
         </div>
         
-        <div id="bgColorBtn">
-                
-            <button id="colorBtn" type="button" onclick="bgColorChange(this)">bigShaq</button>
-            
-        </div>
+        <button id="toggleBtnUppgift5" type="button" onclick="uppgift5()">Uppgift5</button>
 
     </body>
 
